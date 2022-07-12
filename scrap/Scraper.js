519e9350d94f60f74753571f01dc0ed5e4d5892b
milmilkim <soundkim27@gmail.com>
2022-07-13 04:10:10 +0900

    Update Scraper.js

--- scrap/Scraper.js
+++ scrap/Scraper.js
@@ -57,85 +57,80 @@ export const scrapNewBooks = async () => {
     console.log('로그인함');
 
     let pageNum = 1;
-    while (true) {
-      if (pageNum > 2) {
-        break;
+
+    await page.goto(ridiUrl + '&page=' + pageNum);
+
+    const bookList = await page.content();
+    const $ = cheerio.load(bookList);
+
+    /* 제목 */
+    const titleArray = [];
+    const title = $('.title_text.js_highlight_helper');
+    title.each((i, v) => {
+      titleArray[i] = $(v).text().trim();
+    });
+
+    /* 주소 */
+    const urlList = [];
+    const thumbnail_btn = $('a.thumbnail_btn.trackable');
+    const btns = Array.from(thumbnail_btn);
+    btns.map((v, i) => (urlList[i] = 'https://ridibooks.com' + v.attribs.href.split('?')[0]));
+
+    for (let i = 0; i < urlList.length; i++) {
+      //책 개별 데이터
+      await page.goto(urlList[i]);
+      const book = await page.content();
+
+      if (book === null) {
+        throw new Error('데이터가 없음');
       }
 
-      await page.goto(ridiUrl + '&page=' + pageNum);
+      const $ = cheerio.load(book);
 
-      const bookList = await page.content();
-      const $ = cheerio.load(bookList);
+      const data = {};
 
-      /* 제목 */
-      const titleArray = [];
-      const title = $('.title_text.js_highlight_helper');
-      title.each((i, v) => {
-        titleArray[i] = $(v).text().trim();
+      const thumbnail = $('img.thumbnail').attr('src');
+      const title = titleArray[i];
+      const author = [];
+      const authors = $('a.js_author_detail_link.author_detail_link');
+      authors.each((i, v) => {
+        author[i] = $(v).text();
+      }); //작가가 여러명일 수도 있음
+
+      let bookItem = null;
+      bookItem = await getBookItem(title, author);
+
+      if (bookItem !== null) {
+        console.log('이미 있는 책임');
+        await browser.close();
+        await mongoDbClose();
+        return;
+      }
+
+      data.thumbnail = thumbnail;
+      data.title = title;
+      data.author = author;
+      data.url = urlList[i];
+
+      addBook(data); //db에 저장
+
+      const encodedThumbnail = await imageToBase64(`https:${thumbnail}`);
+      const res = await bot.uploadMedia(encodedThumbnail);
+      const imageId = res.data.media_id_string;
+
+      const text = generateTweetText(data.title, data.author, data.url);
+
+      const tweet = await bot.addTweet({
+        text,
+        media: {
+          media_ids: [imageId],
+        },
       });
 
-      /* 주소 */
-      const urlList = [];
-      const thumbnail_btn = $('a.thumbnail_btn.trackable');
-      const btns = Array.from(thumbnail_btn);
-      btns.map((v, i) => (urlList[i] = 'https://ridibooks.com' + v.attribs.href.split('?')[0]));
+      console.log(tweet.data);
+    } //for end
 
-      for (let i = 0; i < urlList.length; i++) {
-        //책 개별 데이터
-        await page.goto(urlList[i]);
-        const book = await page.content();
-
-        if (book === null) {
-          throw new Error('데이터가 없음');
-        }
-
-        const $ = cheerio.load(book);
-
-        const data = {};
-
-        const thumbnail = $('img.thumbnail').attr('src');
-        const title = titleArray[i];
-        const author = [];
-        const authors = $('a.js_author_detail_link.author_detail_link');
-        authors.each((i, v) => {
-          author[i] = $(v).text();
-        }); //작가가 여러명일 수도 있음
-
-        let bookItem = null;
-        bookItem = await getBookItem(title, author);
-
-        if (bookItem !== null) {
-          console.log('이미 있는 책임');
-          await browser.close();
-          await mongoDbClose();
-          return;
-        }
-
-        data.thumbnail = thumbnail;
-        data.title = title;
-        data.author = author;
-        data.url = urlList[i];
-
-        addBook(data); //db에 저장
-
-        const encodedThumbnail = await imageToBase64(`https:${thumbnail}`);
-        const res = await bot.uploadMedia(encodedThumbnail);
-        const imageId = res.data.media_id_string;
-
-        const text = generateTweetText(data.title, data.author, data.url);
-
-        const tweet = await bot.addTweet({
-          text,
-          media: {
-            media_ids: [imageId],
-          },
-        });
-
-        console.log(tweet.data);
-      } //for end
-
-      pageNum += 1;
-    }
+    pageNum += 1;
   } catch (err) {
     console.error(err);
   } finally {
